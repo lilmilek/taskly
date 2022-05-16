6744063dda4870e976514541b48dcb965e40e01c
Milosz <miloszkrainsky@gmail.com>
2022-05-16 15:50:55 +0200

    display collections in navbar menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,20 +2,19 @@
   <div class="navbar bg-base-100">
     <div class="navbar-start">
       <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
+        <div tabindex="0" class="btn btn-ghost btn-circle">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-        </label>
+        </div>
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
           <li class="menu-title pt-2">
             <span>Podstrony</span>
           </li>
           <li><router-link :to="{name: 'home'}">Strona główna</router-link></li>
           <li><router-link :to="{name: 'collections'}">Kolekcje</router-link></li>
-          <li class="menu-title pt-2">
+          <li class="menu-title pt-2" v-if="collections.length">
             <span>Kolekcje</span>
           </li>
-          <li><a>Item 1</a></li>
-          <li><a>Item 2</a></li>
+          <li v-for="collection in collections" :key="collection.id"><router-link :to="{name: 'collection', params: {id: collection.id}}">{{ collection.title }}</router-link></li>
         </ul>
       </div>
     </div>
@@ -56,6 +55,11 @@ import { signOut } from 'firebase/auth'
 import { auth } from '@/firebase/appInit'
 export default {
   name: 'TheNavbar',
+  computed: {
+    collections () {
+      return this.$store.state.collections
+    }
+  },
   methods: {
     signOut () {
       signOut(auth).then(() => {
