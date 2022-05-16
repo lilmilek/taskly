429e8d8e48959f1233f9d69a60b5f7c61ae70bd8
Milosz <miloszkrainsky@gmail.com>
2022-05-16 18:31:41 +0200

    add 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center mt-20 xs:mt-10 px-6">
+    <h1 class="text-[140px] xs:text-[210px] font-extrabold text-primary">404</h1>
+    <h4 class="text-2xl xs:text-3xl font-bold">Coś poszło nie tak...</h4>
+    <p class="text-base-content/40">Nie udało się znaleźć podstrony.</p>
+    <router-link to="/" class="btn btn-primary mt-8">Wróć na stronę główną</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound'
+}
+</script>
+
+<style scoped>
+
+</style>
