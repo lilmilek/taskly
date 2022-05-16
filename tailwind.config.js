c26d66d0060438633ace8919e96bbbf43f7a787a
Milosz <miloszkrainsky@gmail.com>
2022-05-16 15:51:09 +0200

    change tailwind screens size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      xs: '460px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    }
   },
   plugins: [
     // require('@tailwindcss/forms'),
